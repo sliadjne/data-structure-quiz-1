f267f21e6525f37b29c2b79c2fd4f4c20f6e8ed2
sliadjne <144786449+sliadjne@users.noreply.github.com>
2024-05-11 20:59:52 +0700

    contact.java

---
+++ contact.java
@@ -0,0 +1,16 @@
+public class Contact {
+    String name;
+    String phoneNumber;
+    String email;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone: " + phoneNumber + ", Email: " + email;
+    }
+}
